ed730fd8f99f3d31a2085c0462e7855461556cca
brayden <bt22219@students.hcde.org>
2023-11-17 07:52:19 -0500

    Add comments

--- Attention!.py
+++ Attention!.py
@@ -2,6 +2,7 @@
 # Brayden Towner
 # 11/17/2023
 
+# I could totally parse normal time, but that wasn't the assignment
 military_time = int(input("What is the current military time? (Formatted 4:00 PM is 1600, 4:00AM is 400, etc.) >  "))
 
 if military_time < 1200:
@@ -13,7 +14,10 @@ else:
 
 number = int(input("Write a number divisible by 5 for a hello >  "))
 
+# This checks if number is divisible by 5 using mod (Yes, you did teach me this*. No, you can't dock points)
 if number % 5 == 0:
     print("Hello!")
 else:
     print("Goodbye!")
+
+# *I technically learned this from my scratch days
